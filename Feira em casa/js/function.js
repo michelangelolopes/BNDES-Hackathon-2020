28f6a6bc67e81c7c886f2ba76f207cc7ffb6cf37
mick_git <mickeylimalopes@gmail.com>
2020-08-02 00:58:03 -0300

    Atualização 3

---
+++ Feira em casa/js/function.js
@@ -0,0 +1,16 @@
+function setData() {
+    var myObj, myJSON;
+
+    // Storing data:
+    myObj = { name: "John", age: 31, city: "New York" };
+    myJSON = JSON.stringify(myObj);
+    localStorage.setItem("testJSON", myJSON);
+}
+
+function getData() {
+    var myObj, myJSON, text, obj;
+    // Retrieving data:
+    text = localStorage.getItem("testJSON");
+    obj = JSON.parse(text);
+    document.getElementById("demo").innerHTML = obj.name;
+}
